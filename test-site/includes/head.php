3ee20373408ff7318047db338e1b8c69fab42d4b
root <root@Fsociety>
2020-12-18 09:17:21 +0100

    FIXES + Removed some comments because they where useless

--- test-site/includes/head.php
+++ test-site/includes/head.php
@@ -8,27 +8,27 @@
     
     <!-- Favicons Tags -->
     <!-- For changing this, read the README.md in the media/favicons/ -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/media/favicons/xmas/apple-touch-icon.png"> <!-- Supporting apple favicon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="/media/favicons/xmas/favicon-32x32.png"> <!-- Supporting normal favicon 32Bit -->
-    <link rel="icon" type="image/png" sizes="16x16" href="/media/favicons/xmas/favicon-16x16.png"> <!-- Supporting normal favicon 16Bit -->
-    <link rel="manifest" href="/media/favicons/xmas/site.webmanifest"> <!-- Link icon images -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/media/favicons/xmas/apple-touch-icon.png"> 
+    <link rel="icon" type="image/png" sizes="32x32" href="/media/favicons/xmas/favicon-32x32.png"> 
+    <link rel="icon" type="image/png" sizes="16x16" href="/media/favicons/xmas/favicon-16x16.png"> 
+    <link rel="manifest" href="/media/favicons/xmas/site.webmanifest"> 
     <link rel="mask-icon" href="/media/favicons/xmas/safari-pinned-tab.svg" color="#5bbad5">
-    <link rel="shortcut icon" href="/media/favicons/xmas/favicon.ico"> <!-- Supporting normal favicon -->
-    <meta name="msapplication-TileColor" content="#da532c"> <!-- Microsoft menu tile color -->
-    <meta name="msapplication-config" content="/media/favicons/xmas/browserconfig.xml"> <!-- Microsoft menu icon color -->
-    <meta name="theme-color" content="#ffffff"> <!-- Theme color of the icon-->
+    <link rel="shortcut icon" href="/media/favicons/xmas/favicon.ico"> 
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="/media/favicons/xmas/browserconfig.xml"> 
+    <meta name="theme-color" content="#ffffff"> 
 
     <!-- META Tags -->
-    <meta charset="utf-8" /> <!-- default charset, this will suport langauges symbols and chars -->
-    <meta name="author" content="GigaFix" /> <!-- The Creator of the page --> 
-    <meta name="description" content="Computer reparatie GigaFix is er om u de beste service te geven voor de laagste prijs." /> <!-- Information about the page -->
-    <meta property="image" content="media/images/default/og-image.jpg" /> <!-- the image when sharing on media -->
+    <meta charset="utf-8" />
+    <meta name="author" content="GigaFix" /> 
+    <meta name="description" content="Computer reparatie GigaFix is er om u de beste service te geven voor de laagste prijs." /> 
+    <meta property="image" content="media/images/default/og-image.jpg" /> 
 
     <!-- MEDIA META Tags -->
-    <meta property="og:title" content="GigaFix Computer Specialist"> <!-- The title that will be displayed when sharing the site on online media -->
-    <meta property="og:site_name" content="GigaFix"> <!-- The website name that wil be displayed online -->
-    <meta property="og:url" content="https://www.GigaFix.nl/"> <!-- Website url when sharing on online media -->
-    <meta property="og:description" content="Computer reparatie GigaFix is er om u de beste service te geven voor de laagste prijs."> <!-- Website description that shows when sharing online -->
-    <meta property="og:type" content="product"> <!-- Type of tag, used for filtering on platforms -->
-    <meta property="og:image" content="media/favicons/xmas/android-chrome-512x512.png">  <!-- The image displaying with the tag -->
-    <meta property="og:image:alt" content="GigaFix logo." /> <!-- The alt of the image --> 
+    <meta property="og:title" content="GigaFix Computer Specialist"> 
+    <meta property="og:site_name" content="GigaFix">
+    <meta property="og:url" content="https://www.GigaFix.nl/"> 
+    <meta property="og:description" content="Computer reparatie GigaFix is er om u de beste service te geven voor de laagste prijs."> 
+    <meta property="og:type" content="product"> 
+    <meta property="og:image" content="media/favicons/xmas/android-chrome-512x512.png"> 
+    <meta property="og:image:alt" content="GigaFix logo." /> 
--- test-site/scripts/js/animations.js
+++ test-site/scripts/js/animations.js
@@ -1,13 +1,13 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset; // Created the variable that will be needed later
-window.onscroll = function() // When the user starts scrolling it automaticly will run the function
+var prevScrollpos = window.pageYOffset; 
+window.onscroll = function() 
 {
-  var currentScrollPos = window.pageYOffset; // Safes the current position in an variable
-  if (prevScrollpos > currentScrollPos) // Checks if the user is scrolling up then do :
+  var currentScrollPos = window.pageYOffset; 
+  if (prevScrollpos > currentScrollPos) 
   {
-    document.getElementsByTagName("HEADER")[0].style.top = "0"; // Make the header visible
+    document.getElementsByTagName("HEADER")[0].style.top = "0";
   } else {
-    document.getElementsByTagName("HEADER")[0].style.top = "-50px"; // Make the header invisible by swiping it up
+    document.getElementsByTagName("HEADER")[0].style.top = "-50px"; 
   }
-  prevScrollpos = currentScrollPos; // Safes the current scroll to compare it later
+  prevScrollpos = currentScrollPos; 
 } 
