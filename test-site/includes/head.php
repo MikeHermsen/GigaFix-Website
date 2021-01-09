c5352684ece23770cc1123fa75b242f92a0896dd
root <root@Fsociety>
2021-01-09 14:43:30 +0100

    FIXED + Fixed the footer

--- test-site/includes/head.php
+++ test-site/includes/head.php
@@ -10,15 +10,14 @@
     
     <!-- Favicons Tags -->
     <!-- For changing this, read the README.md in the media/favicons/ -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/media/favicons/xmas/apple-touch-icon.png"> 
-    <link rel="icon" type="image/png" sizes="32x32" href="/media/favicons/xmas/favicon-32x32.png"> 
-    <link rel="icon" type="image/png" sizes="16x16" href="/media/favicons/xmas/favicon-16x16.png"> 
-    <link rel="manifest" href="/media/favicons/xmas/site.webmanifest"> 
-    <link rel="mask-icon" href="/media/favicons/xmas/safari-pinned-tab.svg" color="#5bbad5">
-    <link rel="shortcut icon" href="/media/favicons/xmas/favicon.ico"> 
+    <link rel="apple-touch-icon" sizes="180x180" href="/media/favicons/default/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/media/favicons/default/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/media/favicons/default/favicon-16x16.png">
+    <link rel="manifest" href="/media/favicons/default/site.webmanifest">
+    <link rel="shortcut icon" href="/media/favicons/default/favicon.ico">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-config" content="/media/favicons/xmas/browserconfig.xml"> 
-    <meta name="theme-color" content="#ffffff"> 
+    <meta name="msapplication-config" content="/media/favicons/default/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 
     <!-- META Tags -->
     <meta charset="utf-8" />
