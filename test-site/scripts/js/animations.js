b8084bae1e6e412cda0a466f7daad2460976a442
root <root@Fsociety>
2021-01-09 14:30:20 +0100

    FIXED + Fixed the header, Nav & Section animtation
    ADDED 
    + Multi Platform supported Working except the footer.
    
    TODO
    + Fix the Footer for phone like platfroms

--- test-site/scripts/js/animations.js
+++ test-site/scripts/js/animations.js
@@ -5,26 +5,28 @@ window.onscroll = function()
   var currentScrollPos = window.pageYOffset; 
   if (prevScrollpos > currentScrollPos && document.getElementById("container-side-nav").style.width != "100%")  
   {
-    document.getElementsByTagName("HEADER")[0].style.top = "0";
-    document.getElementsByTagName("NAV")[0].style.top = "8.6vh";
+    document.getElementsByTagName("HEADER")[0].style.top = "0px";
+    document.getElementsByTagName("NAV")[0].style.top = "40px";
   } else {
+    document.getElementsByTagName("HEADER")[0].style.top = "-40px";
     document.getElementsByTagName("NAV")[0].style.top = "0px"; 
-    document.getElementsByTagName("SECTION")[0].style.top = "0px"; 
   }
   prevScrollpos = currentScrollPos; 
 } 
 
 
-/* This will make the nav bar popup when clicked on the 3 slides button in the nav */
+/* This will make the nav bar popup when clicked on the 3 lined button in the nav */
 function switchNav() 
 {
   if (document.getElementById("container-side-nav").style.width == "100%") {
       document.getElementById("container-side-nav").style.width = "0";
-      document.getElementsByTagName("HEADER")[0].style.top = "0";
-      document.getElementsByTagName("NAV")[0].style.top = "8.6vh";
+      document.getElementsByTagName("NAV")[0].style.top = "40px";
+      document.getElementsByTagName("HEADER")[0].style.top = "0px";
+
   } else {
       document.getElementById("container-side-nav").style.width = "100%";
       document.getElementsByTagName("NAV")[0].style.top = "0px"; 
-      document.getElementsByTagName("SECTION")[0].style.top = "0px"; 
+      document.getElementsByTagName("HEADER")[0].style.top = "-40px";
   }
 }
+
